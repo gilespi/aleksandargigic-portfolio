d427234a81ceefb1549c43e57bea858a61d36991
Aleksandar <aleksandar12077171@gmail.com>
2020-11-24 21:42:51 +0100

    Create main.js

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+var typed = new Typed('#typed', {
+    strings: ['<strong>Web Designer</strong>', '<strong>Developer</strong>', '<strong>Freelancer</strong>'],
+    typeSpeed: 40,
+    backSpeed: 50,
+    loop: true
+});
